6ca472394412a90bff5c393364947de8b2c01855
Arthur Ascedu <arthurascedu@proton.me>
2025-12-03 10:19:21 +0100

    finished p1

--- p1/install_k3s.sh
+++ p1/install_k3s.sh
@@ -1,6 +1,6 @@
 install_server()
 {
-    curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s -
+    curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --node-ip 192.168.56.110
     sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
 }
 
@@ -8,7 +8,7 @@ install_agent()
 {
     SERVER_TOKEN=$(cat /vagrant/token)
     echo $SERVER_TOKEN
-    curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110 sh -s - agent --token $SERVER_TOKEN
+    curl -sfL https://get.k3s.io | K3S_TOKEN=$SERVER_TOKEN K3S_URL=https://192.168.56.110:6443 sh -s - --node-ip 192.168.56.111
 }
 
 if [ "$1" = "server" ]; then
