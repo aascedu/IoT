9069f63e24b4ad9c1fc7116548801d4b110545b1
Arthur Ascedu <arthurascedu@proton.me>
2025-12-09 14:42:13 +0100

    struggling with the ingress

--- bonus/scripts/setup_bonus.sh
+++ bonus/scripts/setup_bonus.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-CLUSTER_NAME=p3-cluster
+CLUSTER_NAME=bonus-cluster
 
 k3d cluster create $CLUSTER_NAME --api-port 6550 -p "8888:80@loadbalancer" --servers 1 --agents 1
 export KUBECONFIG="$(k3d kubeconfig write k3s-default)"
@@ -10,3 +10,8 @@ kubectl apply -f confs/disable_tls.yml
 kubectl rollout restart deployment argocd-server -n argocd
 kubectl apply -f confs/argocd_ingress.yml
 kubectl apply -f confs/wil_app.yml
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+helm upgrade --install gitlab gitlab/gitlab \
+  -n gitlab \
+  -f /home/aascedu/bonus/gitlab_chart.yml
